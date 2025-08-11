d15ddbc79643ff6d80bd95798c2e5b1db48a573a
Jonathan Morales <moralesjonathan@email.com>
2025-08-11 00:27:08 -0400

    Add Werk it

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 import './App.css'; 
 
 const linkData = [
-  { thumbnail: 'campinn.jpg', title: 'DJ Contest', subtitle: 'Checkout my entry for the Dirtybird DJ Contest!', url: 'https://player-widget.mixcloud.com/widget/iframe/?hide_cover=1&light=1&autoplay=1&feed=%2FJonMoniker%2Fdirtybird-campinn-2025-dj-contest-jon-moniker%2F', ctaText: 'LISTEN', embed: true, embedHeight: '200px' },
+  { thumbnail: 'werkIt.webp', title: 'Werk It', subtitle: 'Newest Collab with Zack & Egosum on Klexos Records!', url: 'https://open.spotify.com/track/3G1Ihf1kMK4H2LlpG0Dz9Z', ctaText: 'STREAM', embed: false },
   { thumbnail: 'GAH.jpg', title: 'GIMME A HUG [EDIT]', subtitle: 'Free edit of Drake\'s GIMME A HUG!', url: 'https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/2037532480&color=%23ff1d00&auto_play=true&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=true', ctaText: 'STREAM/DL', embed: true },
   { thumbnail: 'TFR022.jpg', title: 'Pressure Pleasure', subtitle: 'My latest release on The Forgotten Records!', url: 'https://releases.jonmoniker.com/pressurepleasure', ctaText: 'STREAM', embed: true },
   { thumbnail: 'twistandshout.jpg', title: 'Twist and Shout Bootleg', subtitle: 'Bootleg of the beatles!', url: 'https://releases.jonmoniker.com/twist-and-shout-bootleg', ctaText: 'DOWNLOAD', embed: false },
